248170dbd0865142f337212e0195058761c5be68
makiroggers <makiroggers@gmail.com>
2017-11-23 21:53:17 -0800

    Added tests

--- csharp/strings/ClassLibraryProjects/StringLibraryTest/UnitTest1.cs
+++ csharp/strings/ClassLibraryProjects/StringLibraryTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using UtilityLibraries;
 using Xunit;
 
 namespace StringLibraryTest
@@ -6,17 +7,39 @@ namespace StringLibraryTest
     public class UnitTest1
     {
         [Fact]
-        public static void True(bool? condition, string userMessage)
+        public void TestStartsWithUpper()
         {
-            if (!condition.HasValue || !condition.GetValueOrDefault())
-                throw new TrueException(userMessage, condition);
+            // Tests expected to return true
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+            foreach (var word in words) 
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.True(result);
+                string message = String.Format("Expected for '{0}': true; Actual: {1}", word, result);
+                Console.WriteLine(message);
+            }
         }
 
         [Fact]
-        public static void False(bool? condition, string userMessage)
+        public void TestDoesNotStartWithUpper()
         {
-            if (!condition.HasValue || condition.GetValueOrDefault())
-                throw new FalseException(userMessage, condition);
+            // Tests expected to return false
+            string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство" };
+            foreach (var word in words)
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.False(result);
+                string message = String.Format("Expected for '{0}': false; Actual: {1}", word, result);
+                Console.WriteLine(message);
+            }
         }
+        /*
+        public bool StartsWithUpper(string word)
+        {
+            char c = word[0];
+            bool result = char.IsUpper(word[0]);
+            return result;
+        }
+        */
     }
 }
